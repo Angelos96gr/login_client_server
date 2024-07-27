4e82ce4499e0a23b820331442f16a6fb33e542aa
Angelos96gr <theoharisangelos@gmail.com>
2024-07-28 00:30:50 +0200

    update to support registration of doctors

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request, redirect, session, make_response, url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    session,
+    make_response,
+    url_for,
+)
 from flask_session import Session
 from db_utils import *
 import hashlib
@@ -12,8 +20,8 @@ Session(app)  # defines the flask application to work in the context of sessions
 
 @app.route("/")
 def index():
-    
-    if  (session.get("username") is not None): # user is still logged in
+
+    if session.get("username") is not None:  # user is still logged in
         return render_template("logged_in.html", username=session.get("username"))
 
     return render_template("index.html")
@@ -28,21 +36,28 @@ def home():
 @app.route("/about")
 def about():
     logged_in = False
-    if  (session.get("username") is not None): # user is still logged in
+    if session.get("username") is not None:  # user is still logged in
         logged_in = True
-    
-    return render_template("about.html", logged_in = logged_in, username = session.get("username"))
+
+    return render_template(
+        "about.html", logged_in=logged_in, username=session.get("username")
+    )
 
 
 @app.route("/phonebook")
 def show_docs():
 
     logged_in = False
-    if  (session.get("username") is not None): # user is still logged in
+    if session.get("username") is not None:  # user is still logged in
         logged_in = True
 
     return render_template(
-        "doctors_phonebook.html", doctors_list=get_doctors(), users_list=get_users(), logged_in=logged_in, username= session.get("username"))
+        "doctors_phonebook.html",
+        doctors_list=get_doctors(),
+        users_list=get_users(),
+        logged_in=logged_in,
+        username=session.get("username"),
+    )
 
 
 @app.route("/login", methods=["POST"])
@@ -54,19 +69,23 @@ def login():
         session["pwd"] = request.form.get("pwd")
 
         if check_user_exists(session.get("username"), session.get("pwd")):
-            resp = make_response(render_template("logged_in.html", username=session.get("username")))
-            
+            resp = make_response(
+                render_template("logged_in.html", username=session.get("username"))
+            )
+
             # setting up cookie for tracking which users have logged in
-            cookie_value = hashlib.sha256(session["username"].encode('utf-8')).hexdigest() 
-            resp.set_cookie(f"session_custom_{session.get("username").split("@")[0]}", cookie_value) 
+            cookie_value = hashlib.sha256(
+                session["username"].encode("utf-8")
+            ).hexdigest()
+            resp.set_cookie(
+                f"session_custom_{session.get("username").split("@")[0]}", cookie_value
+            )
             return resp
         else:
             session.clear()
             return render_template(
                 "sign_in_up_result.html", action="login", result="unsucessful"
-
             )
-
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -79,7 +98,7 @@ def signup():
         session["pwd"] = request.form.get("pwd")
 
         if not check_user_exists(session.get("username"), session.get("pwd")):
-            register_new_user(session.get("username"), session.get("pwd"))
+            register_new_user(session.get("username"), session["pwd"])
             result = "succesfull"
         else:
             session.clear()
@@ -97,6 +116,7 @@ def logout():
     session.clear()
     return redirect("/")
 
+
 @app.route("/delete")
 def delete():
     print(f"Deleting user {session.get("username")}")
@@ -107,12 +127,39 @@ def delete():
 
 @app.route("/book_appointment")
 def book():
-    #ToDo to suppot booking apppointments with one of the registered users
+    # ToDo to suppot booking apppointments with one of the registered users
     return redirect("/phonebook")
 
-@app.route("/register_doc")
+
+@app.route("/register_doc", methods=["GET", "POST"])
 def register_doc():
-    logged_in = False
-    if  (session.get("username") is not None): # user is still logged in
-        logged_in = True
-    return render_template("register_doc.html", logged_in = logged_in)
+    if request.method == "GET":
+        logged_in = False
+        if session.get("username") is not None:  # user is still logged in
+            logged_in = True
+
+            # capitalize only first letter
+        return render_template("register_doc.html", logged_in=logged_in)
+    else:
+        result = "unsuccessful"
+        session["full_name"] = " ".join(
+            [request.form.get("first_name"), request.form.get("last_name")]
+        )
+        session["address"] = " ".join(
+            [
+                request.form.get("address_street"),
+                str(request.form.get("address_postal")),
+            ]
+        )
+        session["specialization"] = request.form.get("specialization")
+        if register_new_doctor(
+            session["full_name"],
+            session["address"],
+            session["specialization"],
+            session.get("username"),
+        ):
+            result = "successful"
+
+        return render_template(
+            "sign_in_up_result.html", action="registration", result=result
+        )
--- db_utils.py
+++ db_utils.py
@@ -1,12 +1,15 @@
 import sqlite3
 import pathlib
 import os
+from hashlib import blake2b
 
 PROJECT_PATH = pathlib.Path(__file__).parent.resolve()
 
 
 class Database:
-    def __init__(self, database_name, database_tables, database_cols) -> None:
+    def __init__(
+        self, database_name: str, database_tables, database_cols: list
+    ) -> None:
         self.database_name = database_name
         self.database_tables = database_tables
         self.database_cols = database_cols
@@ -15,23 +18,26 @@ class Database:
         return f"Database object: {self.database_name} with the following tables {self.database_tables}"
 
 
-#Update database instances to have two one for db_users and one for db_doctors
-db_users = Database("users.db", ["user", "docs"], ["user_name", "pwd"])
+# Update database instances to have two one for db_users and one for db_doctors
+db_users = Database("users.db", "user", ["user_name", "pwd"])
+db_doctors = Database(
+    "users.db", "docs", ["full_name", "address", "specialization", "user_id"]
+)
 
 
-def create_new_table(db_name, db_table):
+def create_new_table(db_name, db_table) -> None:
     con = sqlite3.connect(f"{PROJECT_PATH}\\{db_name}")
     cur = con.cursor()
     res = cur.execute(
         f"""CREATE TABLE {db_table} (
-                    doc_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                    full_name TEXT NOT NULL,
-                    address TEXT NOT NULL,
-                    specialization TEXT NOT NULL,
-                    user_id INTEGER UNIQUE NOT NULL,
-                    FOREIGN KEY(user_id) REFERENCES user(user_id)
-                    );
-                    """
+            doc_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            full_name TEXT NOT NULL,
+            address TEXT NOT NULL,
+            specialization TEXT NOT NULL,
+            user_id INTEGER UNIQUE NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES user(user_id)
+            );
+            """
     )
 
 
@@ -42,16 +48,19 @@ def get_data_from_db(db_name, db_table):
     return res
 
 
-def register_new_user(username, pwd):
+def register_new_user(username: str, pwd: str) -> bool:
 
     try:
         con = sqlite3.connect(f"{PROJECT_PATH}\\{db_users.database_name}")
         cur = con.cursor()
-        sql_command = f"INSERT INTO {db_users.database_tables[0]} ({db_users.database_cols[0]},{db_users.database_cols[1]}) VALUES (?,?)"
+        sql_command = f"INSERT INTO {db_users.database_tables} ({db_users.database_cols[0]},{db_users.database_cols[1]}) VALUES (?,?)"
         print(
             f"Adding new user: {username} to database"
         )  # allows duplicated rows because only primary key needs to be unique
-        cur.execute(sql_command, (username, pwd))
+        cur.execute(
+            sql_command,
+            (username, blake2b(pwd.encode("utf-8"), digest_size=4).hexdigest()),
+        )
         con.commit()
         con.close()
         return True
@@ -60,65 +69,90 @@ def register_new_user(username, pwd):
         return False
 
 
-def delete_user(username, pwd):
+def delete_user(username: str, pwd: str) -> bool:
     try:
         con = sqlite3.connect(f"{PROJECT_PATH}\\{db_users.database_name}")
         cur = con.cursor()
-        sql_command = f"DELETE FROM {db_users.database_tables[0]} WHERE {db_users.database_cols[0]} = ? AND {db_users.database_cols[1]} = ?"
+        sql_command = f"DELETE FROM {db_users.database_tables} WHERE {db_users.database_cols[0]} = ? AND {db_users.database_cols[1]} = ?"
         print(f"Deleting user: {username} from database")
-        cur.execute(sql_command, (username, pwd))
+        cur.execute(
+            sql_command,
+            (username, blake2b(pwd.encode("utf-8"), digest_size=4).hexdigest()),
+        )
         con.commit()
         con.close()
         return True
     except:
-        print("Connection with database did not work")
+        print("Connection with database did not work - Did not delete user {username}")
         return False
 
 
-def register_new_doctor(doc_name, doc_address, doc_specialization, user_id):
+def register_new_doctor(
+    doc_name: str, address: str, specialization: str, user_name: str
+) -> bool:
     try:
-        con = sqlite3.connect(f"{PROJECT_PATH}\\{db_users.database_name}")
+        user_list = get_users()
+
+        user_id = [
+            user["user_id"] for user in user_list if user["user_name"] == user_name
+        ]
+
+        con = sqlite3.connect(f"{PROJECT_PATH}\\{db_doctors.database_name}")
         cur = con.cursor()
 
-        #ToDO user_id comes from username i.e. email but should be the primary key instead
-        sql_command = f"""INSERT INTO TABLE {db_users.database_tables[1]} 
-            (full_name, address, specialization, user_id) VALUES(?,?,?,?);"""
-        cur.execute(sql_command, (doc_name, doc_address, doc_specialization, user_id))
-        print(f"Adding new doctor: {doc_name} from database with user_id")
+        sql_command = f"""INSERT INTO {db_doctors.database_tables} 
+            ({db_doctors.database_cols[0]}, {db_doctors.database_cols[1]}, {db_doctors.database_cols[2]}, {db_doctors.database_cols[3]}) VALUES(?,?,?,?);"""
+        cur.execute(sql_command, (doc_name, address, specialization, *user_id))
+        print(
+            f"Adding new doctor: {doc_name} from database with user_id:{user_id} who practises {specialization} in the address {address}"
+        )
         con.commit()
         con.close()
         return True
     except:
-        print("Connection with database did not work")
-        return False                       
+        print("Connection with database did not work - Did not register new doctor")
+        return False
 
-def convert_tuples_dict(res):
 
-    dict_keys = [item[0] for item in res.description]
-    list_tuples = res.fetchall()
-    num_cols = list_tuples[0].__len__()
+def convert_tuples_dict(res) -> list:
 
     list_dict = []
-    for i, item in enumerate(list_tuples):
-        # to do: make number of keys variable depending on the columns in the sql table
-        temp_dict = {}
-        for j in range(num_cols):
-            temp_dict[dict_keys[j]] = item[j]
+    try:
+        dict_keys = [item[0] for item in res.description]
+        list_tuples = res.fetchall()
+        num_cols = list_tuples[0].__len__()
 
-        list_dict.append(temp_dict)
+        for i, item in enumerate(list_tuples):
+            # to do: make number of keys variable depending on the columns in the sql table
+            temp_dict = {}
+            for j in range(num_cols):
+                temp_dict[dict_keys[j]] = item[j]
+
+            list_dict.append(temp_dict)
+    except:
+        print(
+            "Issue with covnerting data in database into list - most likely the database is empty"
+        )
 
     return list_dict
 
 
-def check_user_exists(username, pwd):
+def check_user_exists(username: str, pwd: str) -> bool:
 
     db_entries = convert_tuples_dict(get_data_from_db("users.db", "user"))
-    for db_entry in db_entries:
-        if (db_entry["user_name"] == username) and (db_entry["pwd"] == pwd):
-            print(f"User {username} already exists")
-            return True
-    print(f"User {username} does not exist in the database")
-    return False
+    exists = False
+    if db_entries:
+        for db_entry in db_entries:
+            if (db_entry["user_name"] == username) and (
+                db_entry["pwd"]
+                == blake2b(pwd.encode("utf-8"), digest_size=4).hexdigest()
+            ):
+                print(f"User {username} already exists")
+                exists = True
+                return exists
+        print(f"User {username} does not exist in the database")
+
+    return exists
 
 
 def get_dbs() -> list:
@@ -127,14 +161,14 @@ def get_dbs() -> list:
     return dbs_list
 
 
-def get_users():
+def get_users() -> list:
 
     return convert_tuples_dict(
-        get_data_from_db(db_users.database_name, db_users.database_tables[0])
+        get_data_from_db(db_users.database_name, db_users.database_tables)
     )
 
 
-def get_doctors():
+def get_doctors() -> list:
     return convert_tuples_dict(
-        get_data_from_db(db_users.database_name, db_users.database_tables[1])
+        get_data_from_db(db_users.database_name, db_doctors.database_tables)
     )
