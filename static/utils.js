1348c91842e4456e0a8bfa539c827c51f5da7cc8
Angelos96gr <theoharisangelos@gmail.com>
2024-07-30 09:59:40 +0200

    factored out css styling from html and simplified utils.js

--- static/utils.js
+++ static/utils.js
@@ -1,28 +1,38 @@
 
-document.addEventListener('DOMContentLoaded', function () { // necessary when .js is imported in html otherwise elements are not found
 
-    document.querySelector("[name=signup]").onclick = check_pwd_correctness;
-    document.querySelector("[name=pwd]").onchange = refresh_entries;
-    document.querySelector("[name=pwd_repeat]").onchange = refresh_entries;
+const singup_btn = document.querySelector("[name=signup]");
+const pwd =  document.querySelector("[name=pwd]");
+const pwd_repeat = document.querySelector("[name=pwd_repeat]");
+const incorrect_pwd_msg = document.querySelector("[id=incorrect_pwd]")
+
+
+
+document.addEventListener('DOMContentLoaded', function () { // necessary when .js is imported at top of the html otherwise elements are not found
+
+
+    singup_btn.onclick = check_pwd_correctness;
+    pwd.onchange = refresh_entries;
+    pwd_repeat.onchange = refresh_entries;
 
 
 
 })
 
+
 function refresh_entries(){
 
-    document.querySelector("[id=incorrect_pwd").textContent = ""
+    incorrect_pwd_msg.textContent = ""
 }
 
 
 function check_pwd_correctness() {
-    if (document.getElementsByName("pwd")[0].value != document.getElementsByName("pwd_repeat")[0].value) {
-        document.getElementsByName("pwd_repeat")[0].value = "" //removes second pwd if pwds dont match
-        document.querySelector("[id=incorrect_pwd").textContent = "Passwords do not match"
+    if (pwd.value != pwd_repeat.value) {
+        pwd_repeat.value = "" //removes second pwd if pwds dont match
+        incorrect_pwd_msg.textContent = "Passwords do not match"
     }
     else {
 
-        document.querySelector("[id=incorrect_pwd").textContent = ""
+        incorrect_pwd_msg.textContent = ""
 
     }
 
